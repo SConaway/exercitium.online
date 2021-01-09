c8aeca710f64b6ebb68e3a9b3f235554fbabe192
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-09 23:02:40 +0100

    return jason of gamecode

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from gen import GameCode
+from flask import Flask , jsonify
 
 app =  Flask(__name__)
 
@@ -7,6 +8,12 @@ app =  Flask(__name__)
 def index():
     return "start"
 
+
+@app.route("/game/start")
+def start():
+    gamecode = GameCode.generate()
+    response = {"gamecode":gamecode}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
