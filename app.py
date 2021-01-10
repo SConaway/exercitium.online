6870fd75a28ac471a7271b72f88d7e9ed55b16bc
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2021-01-10 04:33:27 +0000

    feat: serve react frontend with backend

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-import socketio
+import socketio, os
 from gen import GameCode
-from flask import Flask , jsonify
+from flask import Flask , jsonify, redirect, url_for, send_from_directory, abort
 from flask_socketio import SocketIO
 
-app =  Flask(__name__)
+app =  Flask(__name__, static_folder='public')
 app.config["SECRET_KEY"] = "$6Cti2TGf1f4k"
 socketio = SocketIO(app)
 
+@app.route('/dist/<path:path>')
+def dist(path):
+    serve_dir = app.static_folder + '/dist/'
+    if path != "" and os.path.exists(serve_dir + path):
+        send = send_from_directory(serve_dir, path)
+        send.mimetype = 'text/javascript'
+        send.headers['content-type'] = 'text/javascript'
+    else:
+        abort(404)
+    return send
 
-
-
-@app.route("/")
-def index():
-    return "start"
-
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/game/start")
 def start():
