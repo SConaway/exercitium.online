e1a725b9fcefca45f6a2fb052b02c9d68995e3be
Steven Conaway <steven.conaway@icloud.com>
2021-01-10 11:30:48 -0800

    Add teacher socket stuff

--- frontend/src/Teacher.jsx
+++ frontend/src/Teacher.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import {
   Text,
@@ -15,8 +15,21 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
   useToast,
+  Flex,
+  IconButton,
 } from '@chakra-ui/react';
 import { DeleteIcon, PlusSquareIcon } from '@chakra-ui/icons';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './Table';
+
+import io from 'socket.io-client';
 
 export default function Teacher() {
   const [gameID, setGameID] = useState('');
@@ -24,9 +37,41 @@ export default function Teacher() {
   const [questions, setQuestions] = useState(['']);
   const [numTeams, setNumTeams] = useState(2);
 
+  const [connected, setConnected] = useState(false);
+  const [joinedGame, setJoinedGame] = useState(false);
+
+  const socketRef = useRef(null);
+
   const toast = useToast();
 
   const borderColor = useColorModeValue('gray.200', 'gray.600');
+
+  useEffect(() => {
+    const socket = io(import.meta.env['SNOWPACK_PUBLIC_SOCKET.IO_PATH']);
+    socket.on('connect', () => {
+      console.log('connected');
+      setConnected(true);
+    });
+
+    socket.on('disconnect', (reason) => {
+      console.log('disconnected');
+      setConnected(false);
+
+      toast({
+        title: 'Disconnected from server',
+        description: `${reason}`,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    });
+
+    socketRef.current = socket;
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+    //
+  }, []);
 
   const createGame = async () => {
     const response = await fetch('/game/start', {
@@ -36,7 +81,25 @@ export default function Teacher() {
 
     try {
       const json = await response.json();
-      if (json.gamecode) setGameID(json.gamecode);
+      console.log(json);
+      if (json.gamecode) {
+        setGameID(json.gamecode);
+        setJoinedGame(true);
+
+        socketRef.current.emit(
+          'join-session',
+          gameID,
+          // studentName,
+          // studentTeam,
+        );
+
+        socketRef.current.emit(
+          'join-session',
+          `${gameID}-teacher`,
+          // studentName,
+          // studentTeam,
+        );
+      }
     } catch (error) {
       console.error(error);
       toast({
@@ -127,6 +190,65 @@ export default function Teacher() {
 
           <Button size="lg" colorScheme="green" onClick={createGame}>
             Start game!
+          </Button>
+        </VStack>
+      </>
+    );
+
+  if (gameID)
+    content = (
+      <>
+        <VStack spacing="24px">
+          <Text fontSize="lg">Game ID: {gameID}</Text>
+
+          <Table color="gray.500" minWidth="50%" maxWidth="700px">
+            <TableHead>
+              <TableRow>
+                <TableHeader>Team</TableHeader>
+                <TableHeader>Question</TableHeader>
+                <TableHeader>Answer</TableHeader>
+                <TableHeader />
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow bg="white">
+                <TableCell>
+                  <Text fontWeight="bold" fontSize="sm">
+                    3
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Text fontSize="sm" color="gray.500">
+                    What is the craziest thing ever that you've seen? What is
+                    the craziest thing ever that you've seen?
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Text fontSize="sm" color="gray.500">
+                    Spiders
+                  </Text>
+                </TableCell>
+                <TableCell>
+                  <Text fontSize="sm" color="gray.500">
+                    <Flex>
+                      <IconButton
+                        mr="3px"
+                        aria-label="Correct"
+                        icon={<Text>✓</Text>}
+                      />
+                      <IconButton
+                        aria-label="Incorrect"
+                        icon={<Text>𐄂</Text>}
+                      />
+                    </Flex>
+                  </Text>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+          <Button size="lg" colorScheme="green" onClick={createGame}>
+            Next Question
           </Button>
         </VStack>
       </>
