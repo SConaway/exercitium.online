ef99111dd1980862e221a35eec5051510828dbc3
s1ntaxe770r <cyberdev01@protonmail.com>
2021-01-10 08:27:52 +0100

    rough join room implementation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import socketio
 from gen import GameCode
 from flask import Flask , jsonify
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO , join_room
 
 app =  Flask(__name__)
 app.config["SECRET_KEY"] = "$6Cti2TGf1f4k"
@@ -21,9 +21,11 @@ def start():
     response = {"gamecode":gamecode}
     return jsonify(response)
 
-@socketio.on('join')
-def join(gamedetails):
-    print('received json: ' + str(gamedetails))
+
+
+# @socketio.on('join-session')
+# def handle_join(gamecode):
+#     join_room()
 
 if __name__ == "__main__":
    socketio.run(app)
